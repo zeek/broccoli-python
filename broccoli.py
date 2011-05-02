b86ceb6e57c6c1662da854fafb27cd3c48f98a6a
Robin Sommer <robin@icir.org>
2011-05-02 08:34:53 -0700

    Making python-broccoli work with 64-bit integers.

--- broccoli.py
+++ broccoli.py
@@ -1,7 +1,7 @@
 
 import socket
 import struct
-from types import FunctionType 
+from types import FunctionType
 
 from _broccoli_intern import *
 
@@ -15,12 +15,12 @@ class Connection:
         self.destination = destination
         if not self.bc:
             raise IOError("cannot init Broccoli connection handle")
-        
+
         self._registerHandlers()
-        
+
         if broclass:
             bro_conn_set_class(self.bc, broclass)
-        
+
         if connect:
             self.connect();
 
@@ -28,7 +28,7 @@ class Connection:
     def connect(self):
         if not bro_conn_connect(self.bc):
             raise IOError("cannot connect to %s" % self.destination)
-        
+
     # Hand control to Broccoli's I/O loop.
     # Returns true if the send queue is non-empty.
     def processInput(self):
@@ -40,11 +40,11 @@ class Connection:
         ev = bro_event_new(name)
         for arg in args:
             bro_event_add_val(ev, _getInternalVal(arg));
-            
+
         bro_event_send(self.bc, ev);
         bro_event_free(ev);
         self.processInput()
-        
+
     # Explicit subscribe
     def subscribe(self, event_name, callback):
         ev = event(callback)
@@ -55,14 +55,14 @@ class Connection:
         for ev in _Events:
             bro_event_registry_add_compact(self.bc, ev.__name__, ev);
 
-            
-##### Wrapped helper functions.        
+
+##### Wrapped helper functions.
 def current_time():
     return bro_util_current_time()
 
 ##### Decorator @event(val-type1, val-type2, val-type3, ...)
 
-# List of all functions declared with the @event decorator 
+# List of all functions declared with the @event decorator
 # (more precisely, list of all of their wrappers; see below).
 _Events = []
 
@@ -71,32 +71,32 @@ def event(*types):
     # We wrap the event callback into a function which turns the 2-tuples (type,val)
     # that we get from the C layer into the corresponding Python object.
     def make_wrapper(func):
-        
+
         def wrapped_f(*args):
-            
+
 	    new_args = []
-            
+
             ptypes = types
             if not ptypes:
-                # Allow omitting types. 
+                # Allow omitting types.
                 ptypes =  [None] *len(args)
-                
+
             for (arg, type) in zip(args, ptypes):
                 # Split the 2-tuples passed to us by the C layer.
                 (btype, val) = arg
                 # Create an instance of the corresponding Python type.
 		new_args += [instantiate(btype, val, type)]
-                
+
             # Finally call the callback.
 	    return func(*new_args);
 
         # Pretend the wrapper has the name of the actual callback (rather than "wrapped_f" ...)
 	wrapped_f.func_name = func.func_name
-	
+
         # Add the wrapped function to the list of events handlers.
         global _Events
         _Events += [wrapped_f]
-            
+
         return wrapped_f
 
     # Allow @event instead of @event()
@@ -104,7 +104,7 @@ def event(*types):
         func = types[0]
         types = ()
         return make_wrapper(func)
-    
+
     else:
         return make_wrapper
 
@@ -115,23 +115,23 @@ def event(*types):
 
 # Base class for our wrapper classes.
 # For atomic types, the classes here act as both type and instances. For non-atomic
-# types (i.e., records) we define separate type and instance classes below. 
+# types (i.e., records) we define separate type and instance classes below.
 class Val:
     # Type is the Bro type BRO_TYPE_*.
-    # Val is representation of the Val in a standard Python type. 
+    # Val is representation of the Val in a standard Python type.
     def __init__(self, type, val):
         self.type = type
         self.val = val
-        
+
         self.__class__._bro_type = type # Doing it once would be sufficient.
 
     def __str__(self):
 	return str(self.val)
-	
+
     # Convert value into a 2-tuple (type, val) as expected by the C layer.
     def internalVal(self):
         return (self.type, self.val)
-    
+
 class count(Val):
     def __init__(self, val):
         Val.__init__(self, BRO_TYPE_COUNT, int(val))
@@ -147,7 +147,7 @@ class count(Val):
 class interval(Val):
     def __init__(self, val):
         Val.__init__(self, BRO_TYPE_INTERVAL, float(val))
-	
+
     @staticmethod
     def _factory(val, dst_type):
         v = interval(val)
@@ -159,7 +159,7 @@ class interval(Val):
 class time(Val):
     def __init__(self, val):
         Val.__init__(self, BRO_TYPE_TIME, float(val))
-	
+
     @staticmethod
     def _factory(val, dst_type):
         v = time(val)
@@ -169,10 +169,10 @@ class time(Val):
         return v
 
 class port(Val):
-    
+
     protos_by_name = { "tcp": 6, "udp": 17, "icmp": 1 }
     protos_by_num =  { 6: "tcp", 17: "udp", 1: "icmp" }
-    
+
     def __init__(self, str=None, internal=None):
         v = internal and internal or self._parse(str)
         Val.__init__(self, BRO_TYPE_PORT, v)
@@ -183,7 +183,7 @@ class port(Val):
             return "%d/%s" % (port, self.protos_by_num[proto])
         except IndexError:
             return "%s/unknown" % port
-        
+
     @staticmethod
     def _factory(val, dst_type):
         v = port(internal=val)
@@ -193,14 +193,14 @@ class port(Val):
             return v[0]
         _typeCheck(dst_type, port)
         return v
-    
+
     def _parse(self, str):
         (port, proto) = str.split("/")
         try:
             return (int(port), self.protos_by_name[proto.lower()])
         except (IndexError, ValueError):
             return (0, 0)
-        
+
 class addr(Val):
     def __init__(self, str=None, internal=None):
         v = internal and internal or self._parse(str)
@@ -208,7 +208,7 @@ class addr(Val):
 
     def __str__(self):
         return socket.inet_ntoa(struct.pack('=l', self.val))
-        
+
     @staticmethod
     def _factory(val, dst_type):
         v = addr(internal=val)
@@ -216,7 +216,7 @@ class addr(Val):
             return str(v)
         _typeCheck(dst_type, addr)
         return v
-    
+
     def _parse(self, str):
         return struct.unpack('=l',socket.inet_aton(str))[0]
 
@@ -228,7 +228,7 @@ class net(Val):
 
     def __str__(self):
         return "X.X.X"  # FIXME
-        
+
     @staticmethod
     def _factory(val, dst_type):
         v = net(internal=val)
@@ -236,7 +236,7 @@ class net(Val):
             return str(v)
         _typeCheck(dst_type, net)
         return v
-    
+
     def _parse(self, str):
         return 0   # FIXME
 
@@ -248,7 +248,7 @@ class subnet(Val):
     def __str__(self):
         (net, mask) = self.val
         return "%s/%d" % (socket.inet_ntoa(struct.pack('=l', net)), mask)
-    
+
     @staticmethod
     def _factory(val, dst_type):
         v = subnet(internal=val)
@@ -256,7 +256,7 @@ class subnet(Val):
             return str(v)
         _typeCheck(dst_type, subnet)
         return v
-    
+
     def _parse(self, str):
         (net, mask) = str.split("/")
         return (struct.unpack('=l',socket.inet_aton(net))[0], int(mask))
@@ -270,22 +270,22 @@ class enum(Val):
 
     def __str__(self):
         return "XXXX"  # FIXME
-        
+
     @staticmethod
     def _factory(val, dst_type):
         v = enum(internal=val)
         _typeCheck(dst_type, enum)
         return v
-    
+
     def _parse(self, str):
         return 0   # FIXME
-    
-# Helper class for unset values.    
-class unknown(Val):        
+
+# Helper class for unset values.
+class unknown(Val):
     def __init__(self):
         Val.__init__(self, BRO_TYPE_UNKNOWN, None)
 
-# Dictionary of all defined record types.         
+# Dictionary of all defined record types.
 RecTypes = {}
 
 # Type class for records, which maps field names to indices.
@@ -293,7 +293,7 @@ RecTypes = {}
 class record_type:
     def __init__(self, *fields):
         self.fields = fields
-	
+
 	global RecTypes
         # Remember this type by its name.
 	RecTypes[self.__class__.__name__] = self
@@ -303,20 +303,20 @@ class record_type:
         # FIXME: Add _typeCheck(),
         # FIXME: For recursive records we'd need to pass the right record type
         # here instead of none, which we don't have. How to do that?
-        
+
         # Get the type.
         rec_type = RecTypes[dst_type.__class__.__name__]
-        
+
         # Init the field values.
         vals = [instantiate(btype, val, None) for (btype, val) in vals]
-        
+
 	return record(rec_type, vals)
 
 # Class for record instances.
 class record(Val):
     def __init__(self, type, vals = None):
         Val.__init__(self, BRO_TYPE_RECORD, {})
-        
+
         # Save the record's type.
 	self._type = type
 
@@ -327,7 +327,7 @@ class record(Val):
         # Initialize record fields.
         for (key, val) in zip(type.fields, vals):
             self.val[key] = val
-                
+
     def internalVal(self):
         vals = [_getInternalVal(self.val.get(f, unknown())) for f in self._type.fields]
         return (BRO_TYPE_RECORD, vals)
@@ -337,7 +337,7 @@ class record(Val):
         if "_type" in self.__dict__ and key in self._type.fields:
             return self.val[key]
         raise AttributeError
-        
+
     def __setattr__(self, key, val):
         try:
             if key in self._type.fields:
@@ -362,7 +362,7 @@ def _typeCheck(type1, type2):
         if type == str:
             return BRO_TYPE_STRING;
         return type._bro_type
-    
+
     if type1 and type2 and typeToBro(type1) != typeToBro(type2):
         raise TypeError
 
@@ -382,10 +382,10 @@ def _getInternalVal(arg):
         return(BRO_TYPE_DOUBLE, arg)
     else:
         return arg.internalVal()
-    
+
 # Factories for Python internal types.
-def _int_factory(val, dst_type):
-    return int(val)
+def _long_factory(val, dst_type):
+    return long(val)
 
 def _bool_factory(val, dst_type):
     return bool(val)
@@ -396,13 +396,13 @@ def _string_factory(val, dst_type):
 def _float_factory(val, dst_type):
     return float(val)
 
-string = str    
+string = str
 double = float
 
 # Table of factories for all supported types so that we can dynamically
 # instantiate them.
 _Factories = {
-    BRO_TYPE_INT: _int_factory,
+    BRO_TYPE_INT: _long_factory,
     BRO_TYPE_BOOL: _bool_factory,
     BRO_TYPE_COUNT: count._factory,
     BRO_TYPE_TIME: time._factory,
@@ -420,6 +420,6 @@ _Factories = {
 def instantiate(src_type, val, dst_type):
     return _Factories[src_type](val, dst_type)
 
-        
+
 
 
