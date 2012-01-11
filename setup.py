b408cc8fc5cbb5f8ad1284d422eb501793cf7a2f
Robin Sommer <robin@icir.org>
2012-01-10 16:56:13 -0800

    Updating CHANGES and VERSION.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from distutils.core import setup, Extension
 
 setup(name="broccoli-python", 
-    version="0.51-7", # Filled in automatically.
+    version="0.51-16", # Filled in automatically.
     author_email="info@bro-ids.org",
     license="BSD",
     py_modules=['broccoli'],
