d97ec2e2041e754598241ed594123b21cb1d5ecd
Johanna Amann <johanna@icir.org>
2016-08-12 13:14:12 -0700

    Updating CHANGES and VERSION.
     [nomail]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from distutils.core import setup, Extension
 
 setup(name="broccoli-python",
-    version="0.59", # Filled in automatically.
+    version="0.60", # Filled in automatically.
     author_email="info@bro.org",
     license="BSD",
     py_modules=['broccoli'],
